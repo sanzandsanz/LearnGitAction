267d6494f3a09b7c5f7bfdc4a5c19bace0edf33d
Sanjay <sanjay.bajracharya@aremedia.com.au>
2025-07-27 14:49:56 +1000

    add test project

---
+++ LearnGitAction.Tests/CalculationServiceTests.cs
@@ -0,0 +1,18 @@
+﻿namespace LearnGitAction.Tests;
+
+public class CalculationServiceTests
+{
+    [Fact]
+    public void AddTwoAndThreeShouldReturnFive()
+    {
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        var result = CalculationService.Add(a, b);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
